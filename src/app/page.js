5082ebe3b1d6cc89696fbd5b62b3b0fc181c5e43
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2025-06-05 02:02:17 +0100

    Moved search bar to replace job title heading Changed search bar styling Changed searchbar input to Radix input Setup classes for job rows based on application status Added unused switch to jobs box

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import AddJobForm from "@/components/AddJobForm/AddJobForm";
+import AddJobSwitch from "@/components/AddJobForm/AddJobSwitch";
 import AllJobsCard from "@/components/AllJobsCard/AllJobsCard";
 
 export default function HomePage() {
   return (
     <div>
-      <AddJobForm />
+      <AddJobSwitch />
       <AllJobsCard />
     </div>
   );
